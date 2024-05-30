d3ff640a8cd15dfc978791ab0eb3772e9f20aea5
Mbali <mbalimaduna76@gmail.com>
2024-05-30 13:13:20 +0200

    update config file

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import handlebars from 'vite-plugin-handlebars';
 import data from './data.json';
 
-export default {
-  plugins: [
-    handlebars({
-      context: {}, // replace the curly brackets with data.json
-    }),
-  ],
+  export default {
+    base : '/responsive_cv/',   // <-- add this
+    plugins: [handlebars({
+        context : data   // add this
+    })],
 };
+
